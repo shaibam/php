5e95565a0f551ecfc93681256a36d0b28f76bb09
shai kimchi <shai.k@diglobal.tech>
2022-08-26 22:57:25 +0300

    phps

--- save-transaction.php
+++ save-transaction.php
@@ -1,8 +1,8 @@
 <?php
     if(isset($_POST)) {
         $data = json_decode(file_get_contents("php://input"));
-        if (!$data->account) {
-            deliver_response(400, "account missing", $data);
+        if (!$data->url && !$data->transaction) {
+            deliver_response(400, "data missing", $data);
         } else {
             $username="udoimzgcuyxfd";
             $password="lfapts0vjgny";
@@ -10,10 +10,16 @@
             $mysqli = new mysqli("localhost", $username, $password, $database);
             $mysqli->select_db($database) or die( "Unable to select database");
 
-            $account = $data->account;
-            $transactions = $data->transactions;
+            $url = $data->url;
+            $transaction = $data->transaction;
 
-            echo $transactions;
+            if ($transaction) {
+                $update_transaction_query="UPDATE metadata SET receipt = '{$transaction}', status = 'minted' WHERE url='{$url}'";
+                $result_update = $mysqli->query("$update_transaction_query"); 
+                deliver_response(200, "success",  $transaction );
+            } else {
+                deliver_response(500,"fail","");
+            }
         }
     }
    
